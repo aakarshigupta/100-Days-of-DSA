ba343d1bbb6e1bb74908f6a98e3ffacdf446db9c
Aakarshi Gupta <aakarshigupta389@gmail.com>
2026-03-19 06:09:00 +0000

    DAY 47 OF 100 DAYS OF DSA

---
+++ day47.c
@@ -0,0 +1,83 @@
+/*Problem: Height of Binary Tree
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int value) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = value;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* buildTree(int arr[], int n) {
+
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = newNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    int j = 1;
+
+    for (int i = 0; i < n && j < n; i++) {
+        if (nodes[i] != NULL) {
+
+            if (j < n)
+                nodes[i]->left = nodes[j++];
+
+            if (j < n)
+                nodes[i]->right = nodes[j++];
+        }
+    }
+
+    return nodes[0];
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return 1 + (left > right ? left : right);
+}
+
+int main() {
+
+    int n;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter level order traversal (-1 for NULL):\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    printf("Height of Binary Tree: %d\n", height(root));
+
+    return 0;
+}
